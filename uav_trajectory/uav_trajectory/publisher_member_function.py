25134146775196fedbdb9b315824327ca54968f6
tschweitzer57 <thibault.schweitzer@estaca.eu>
2023-11-15 17:03:20 +0000

    Ajout de matatrices 4x4 aux messages

--- uav_trajectory/uav_trajectory/publisher_member_function.py
+++ uav_trajectory/uav_trajectory/publisher_member_function.py
@@ -1,54 +1,89 @@
-# Copyright 2016 Open Source Robotics Foundation, Inc.
+# Licensed under CECILL-2.1
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Author : thibault SCHWEITZER
+# email :  thibault.schweitzer@estaca.eu
 
 import rclpy
+import numpy as np
 from rclpy.node import Node
 
-from tutorial_interfaces.msg import Groundtruth
+from uav_interfaces.msg import Groundtruth
+from uav_interfaces.msg import VIOmeasurement
+from uav_interfaces.msg import UWBmeasurement
+from uav_interfaces.msg import VDmeasurement
+from uav_interfaces.msg import LCmeasurement
 
-
-class MinimalPublisher(Node):
+class UavPublisher(Node):
 
     def __init__(self):
-        super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(Groundtruth, 'topic', 10)
-        timer_period = 0.5  # seconds
+        super().__init__('uav_publisher')
+        self.publisher_gt = self.create_publisher(Groundtruth,    'topic1', 10)
+        self.publisher_vio = self.create_publisher(VIOmeasurement, 'topic2', 10)
+        self.publisher_uwb = self.create_publisher(UWBmeasurement, 'topic3', 10)
+        self.publisher_vd = self.create_publisher(VDmeasurement,  'topic4', 10)
+        self.publisher_lc = self.create_publisher(LCmeasurement,  'topic5', 10)
+        timer_period = 1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
+        self.npdata = np.array([1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,11.0,12.0,13.0,14.0,15.0,16.0])
 
     def timer_callback(self):
-        msg = Groundtruth()
-        msg.frame_id = self.i
-        msg.x = 0.125
-        msg.y = 0.453
-        msg.z = 0.875
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: frame: %d \t x: %d \t y: %d \t z: %d' % (msg.frame_id, msg.x, msg.y, msg.z))
+        self.callback_gt()
+        self.callback_vio()
+        self.callback_uwb()
+        self.callback_vd()
+        self.callback_lc()
         self.i += 1
 
+    def callback_gt(self):
+        msg = Groundtruth()
+        msg.frame_id = self.i
+        msg.x = 0.891
+        msg.y = 0.453
+        msg.z = 0.456
+        msg.psi = 0.5455545
+        self.publisher_gt.publish(msg)
+        self.get_logger().info('Publishing: groundtruth infos')
+
+    def callback_vio(self):
+        msg = VIOmeasurement()
+        msg.frame_id = self.i
+        msg.measure = self.npdata
+        self.publisher_vio.publish(msg)
+        self.get_logger().info('Publishing: VIO infos')
+
+    def callback_uwb(self):
+        msg = UWBmeasurement()
+        msg.frame_id = self.i
+        msg.measure = 58.45425
+        self.publisher_uwb.publish(msg)
+        self.get_logger().info('Publishing: UWB infos')
+
+    def callback_vd(self):
+        msg = VDmeasurement()
+        msg.frame_id = self.i
+        msg.measure = self.npdata
+        self.publisher_vd.publish(msg)
+        self.get_logger().info('Publishing: VD infos')
+
+    def callback_lc(self):
+        msg = LCmeasurement()
+        msg.frame_id = self.i
+        msg.measure = self.npdata
+        self.publisher_lc.publish(msg)
+        self.get_logger().info('Publishing: LC infos')
 
 def main(args=None):
     rclpy.init(args=args)
 
-    minimal_publisher = MinimalPublisher()
+    uav_publisher = UavPublisher()
 
-    rclpy.spin(minimal_publisher)
+    rclpy.spin(uav_publisher)
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
+    uav_publisher.destroy_node()
     rclpy.shutdown()
 
 
--- uav_trajectory/uav_trajectory/subscriber_member_function.py
+++ uav_trajectory/uav_trajectory/subscriber_member_function.py
@@ -1,51 +1,59 @@
-# Copyright 2016 Open Source Robotics Foundation, Inc.
+# Licensed under CECILL-2.1
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Author : thibault SCHWEITZER
 
 import rclpy
 from rclpy.node import Node
 
-from tutorial_interfaces.msg import Groundtruth
+from uav_interfaces.msg import Groundtruth
+from uav_interfaces.msg import VIOmeasurement
+from uav_interfaces.msg import UWBmeasurement
+from uav_interfaces.msg import VDmeasurement
+from uav_interfaces.msg import LCmeasurement
 
-
-class MinimalSubscriber(Node):
+class UavSubscriber(Node):
 
     def __init__(self):
-        super().__init__('minimal_subscriber')
-        self.subscription = self.create_subscription(
-            Groundtruth,
-            'topic',
-            self.listener_callback,
-            10)
-        self.subscription  # prevent unused variable warning
+        super().__init__('uav_subscriber')
+        self.subscription_gt = self.create_subscription(Groundtruth,'topic1',self.listener_callback_gt,10)
+        self.subscription_gt  # prevent unused variable warning
+        self.subscription_vio = self.create_subscription(VIOmeasurement,'topic2',self.listener_callback_vio,10)
+        self.subscription_vio 
+        self.subscription_uwb = self.create_subscription(UWBmeasurement,'topic3',self.listener_callback_uwb,10)
+        self.subscription_uwb 
+        self.subscription_vd = self.create_subscription(VDmeasurement,'topic4',self.listener_callback_vd,10)
+        self.subscription_vd 
+        self.subscription_lc = self.create_subscription(LCmeasurement,'topic5',self.listener_callback_lc,10)
+        self.subscription_lc
 
-    def listener_callback(self, msg):
-        self.get_logger().info('I heard: frame: %d \t x: %d \t y: %d \t z: %d' % (msg.frame_id, msg.x, msg.y, msg.z))
+    def listener_callback_gt(self, msg):
+        self.get_logger().info('I heard: Groundtruth frame: %d' % msg.frame_id)
+
+    def listener_callback_vio(self, msg):
+        self.get_logger().info('I heard: VIO frame: %d' % msg.frame_id)
+
+    def listener_callback_uwb(self, msg):
+        self.get_logger().info('I heard: UWB frame: %d' % msg.frame_id)
+
+    def listener_callback_vd(self, msg):
+        self.get_logger().info('I heard: VD frame: %d' % msg.frame_id)
+
+    def listener_callback_lc(self, msg):
+        self.get_logger().info('I heard: LC data 1: %f' % msg.measure[1])
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    minimal_subscriber = MinimalSubscriber()
+    uav_subscriber = UavSubscriber()
 
-    rclpy.spin(minimal_subscriber)
+    rclpy.spin(uav_subscriber)
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    minimal_subscriber.destroy_node()
+    uav_subscriber.destroy_node()
     rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
