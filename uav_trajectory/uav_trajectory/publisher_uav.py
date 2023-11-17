8631f058974bb75ac6dab83d0984d14b97b29837
tschweitzer57 <thibault.schweitzer@estaca.eu>
2023-11-17 17:28:48 +0100

    ajout code VIO

--- uav_trajectory/uav_trajectory/publisher_uav.py
+++ uav_trajectory/uav_trajectory/publisher_uav.py
@@ -9,7 +9,7 @@ from rclpy.node import Node
 from uav_trajectory.uavlib import *
 
 from uav_interfaces.msg import Groundtruth
-# from uav_interfaces.msg import VIOmeasurement
+from uav_interfaces.msg import VIOmeasurement
 # from uav_interfaces.msg import UWBmeasurement
 # from uav_interfaces.msg import VDmeasurement
 # from uav_interfaces.msg import LCmeasurement
@@ -19,32 +19,39 @@ class UavPublisher(Node):
     def __init__(self):
         super().__init__('uav_publisher')
         self.publisher_gt = self.create_publisher(Groundtruth,    'topic1', 10)
-        # self.publisher_vio = self.create_publisher(VIOmeasurement, 'topic2', 10)
+        self.publisher_vio = self.create_publisher(VIOmeasurement, 'topic2', 10)
         # self.publisher_uwb = self.create_publisher(UWBmeasurement, 'topic3', 10)
         # self.publisher_vd = self.create_publisher(VDmeasurement,  'topic4', 10)
         # self.publisher_lc = self.create_publisher(LCmeasurement,  'topic5', 10)
-        timer_period = 1  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+
         # Generates trajectory for UAV A
         self.uavA = UAV(initialPose=[0,0,0])
         self.uavA.generate_3DoF_trajectory(diameter = 30)
-
         # Generates trajectory for UAV B
         self.uavB = UAV(initialPose=[40,0,0])
         self.uavB.generate_3DoF_trajectory(diameter = 30)
-
         # Generates trajectory for UAV C
         self.uavC = UAV(initialPose=[0,40,0])
         self.uavC.generate_3DoF_trajectory(diameter = 30)
-
         # Generates trajectory for UAV D
         self.uavD = UAV(initialPose=[40,40,0])
         self.uavD.generate_3DoF_trajectory(diameter = 30)
+        
+        # generate VIO data for the drones :
+        vio_noise = 0.01
+        self.uavA.generate_VIO(vio_noise)
+        self.uavB.generate_VIO(vio_noise)
+        self.uavC.generate_VIO(vio_noise)
+        self.uavD.generate_VIO(vio_noise)
+        
+        timer_period = 1  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+        
 
     def timer_callback(self):
         self.callback_gt()
-        # self.callback_vio()
+        self.callback_vio()
         # self.callback_uwb()
         # self.callback_vd()
         # self.callback_lc()
@@ -56,16 +63,16 @@ class UavPublisher(Node):
         msg.x = self.uavC.groundtruth_x[self.i]
         msg.y = self.uavC.groundtruth_y[self.i]
         msg.z = self.uavC.groundtruth_z[self.i]
-        msg.psi = self.uavC.groundtruth_x[self.i]
+        msg.psi = 0.0
         self.publisher_gt.publish(msg)
         self.get_logger().info('Publishing: groundtruth infos')
 
-    # def callback_vio(self):
-        # msg = VIOmeasurement()
-        # msg.frame_id = self.i
-        # msg.measure = self.npdata
-        # self.publisher_vio.publish(msg)
-        # self.get_logger().info('Publishing: VIO infos')
+    def callback_vio(self):
+        msg = VIOmeasurement()
+        msg.frame_id = self.i
+        msg.measure = np.reshape(self.uavA.z_VIO[self.i], (1,16))
+        self.publisher_vio.publish(msg)
+        self.get_logger().info('Publishing: VIO infos')
 
     # def callback_uwb(self):
         # msg = UWBmeasurement()
