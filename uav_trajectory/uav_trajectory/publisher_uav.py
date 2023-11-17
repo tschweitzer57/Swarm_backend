f1973c909b1aaab4620335b7714f3ca0e4aaf3cb
tschweitzer57 <thibault.schweitzer@estaca.eu>
2023-11-17 23:32:06 +0100

    Debug vio 4

--- uav_trajectory/uav_trajectory/publisher_uav.py
+++ uav_trajectory/uav_trajectory/publisher_uav.py
@@ -70,7 +70,8 @@ class UavPublisher(Node):
     def callback_vio(self):
         msg = VIOmeasurement()
         msg.frame_id = self.i
-        msg.measure = np.reshape(self.uavA.z_VIO[self.i].T, (1,16))
+        print(np.reshape(self.uavA.z_VIO[self.i].T, (1,16)))
+        msg.measure = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16])
         self.publisher_vio.publish(msg)
         self.get_logger().info('Publishing: VIO infos')
 
