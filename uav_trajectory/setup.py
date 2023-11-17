f41a1cc81de585eca100abf7ff217a535376ebd0
tschweitzer57 <thibault.schweitzer@estaca.eu>
2023-11-17 16:00:06 +0100

    test avec 2 init

--- uav_trajectory/setup.py
+++ uav_trajectory/setup.py
@@ -5,7 +5,7 @@ package_name = 'uav_trajectory'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[find_packages(exclude=['test']), uavlib],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
