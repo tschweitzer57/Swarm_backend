76b4c77cf68cf2beea52e0b5ba3c381e9ad36bef
tschweitzer57 <thibault.schweitzer@estaca.eu>
2023-11-19 00:51:15 +0100

    amelioration des interfaces uav et ajout détection visuelle et loop closure

--- uav_trajectory/uav_trajectory/publisher_uav.py
+++ uav_trajectory/uav_trajectory/publisher_uav.py
@@ -11,8 +11,8 @@ from uav_trajectory.uavlib import *
 from uav_interfaces.msg import Groundtruth
 from uav_interfaces.msg import VIOmeasurement
 from uav_interfaces.msg import UWBmeasurement
-# from uav_interfaces.msg import VDmeasurement
-# from uav_interfaces.msg import LCmeasurement
+from uav_interfaces.msg import VDmeasurement
+from uav_interfaces.msg import LCmeasurement
 
 class UavPublisher(Node):
 
@@ -21,8 +21,8 @@ class UavPublisher(Node):
         self.publisher_gt = self.create_publisher(Groundtruth,    'topic1', 10)
         self.publisher_vio = self.create_publisher(VIOmeasurement, 'topic2', 10)
         self.publisher_uwb = self.create_publisher(UWBmeasurement, 'topic3', 10)
-        # self.publisher_vd = self.create_publisher(VDmeasurement,  'topic4', 10)
-        # self.publisher_lc = self.create_publisher(LCmeasurement,  'topic5', 10)
+        self.publisher_vd = self.create_publisher(VDmeasurement,  'topic4', 10)
+        self.publisher_lc = self.create_publisher(LCmeasurement,  'topic5', 10)
 
         # Generates trajectory for UAV A
         self.uavA = UAV(initialPose=[0,0,0])
@@ -51,6 +51,20 @@ class UavPublisher(Node):
         self.uavC.generate_UWB(self.uavD, self.uavA, self.uavB, uwb_noise)
         self.uavD.generate_UWB(self.uavA, self.uavB, self.uavC, uwb_noise)
         
+        # generate visual detection data for the drones :
+        vd_noise = 0.01
+        self.uavA.generate_VD(self.uavB, self.uavC, self.uavD, vd_noise)
+        self.uavB.generate_VD(self.uavC, self.uavD, self.uavA, vd_noise)
+        self.uavC.generate_VD(self.uavD, self.uavA, self.uavB, vd_noise)
+        self.uavD.generate_VD(self.uavA, self.uavB, self.uavC, vd_noise)
+        
+        # generate visual detection data for the drones :
+        lc_noise = 0.01
+        self.uavA.generate_LC(self.uavB, self.uavC, self.uavD, lc_noise)
+        self.uavB.generate_LC(self.uavC, self.uavD, self.uavA, lc_noise)
+        self.uavC.generate_LC(self.uavD, self.uavA, self.uavB, lc_noise)
+        self.uavD.generate_LC(self.uavA, self.uavB, self.uavC, lc_noise)
+        
         timer_period = 1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
@@ -60,8 +74,8 @@ class UavPublisher(Node):
         self.callback_gt()
         self.callback_vio()
         self.callback_uwb()
-        # self.callback_vd()
-        # self.callback_lc()
+        self.callback_vd()
+        self.callback_lc()
         self.i += 1
 
     def callback_gt(self):
@@ -90,19 +104,88 @@ class UavPublisher(Node):
         self.publisher_uwb.publish(msg)
         self.get_logger().info('Publishing: UWB infos')
 
-    # def callback_vd(self):
-        # msg = VDmeasurement()
-        # msg.frame_id = self.i
-        # msg.measure = self.npdata
-        # self.publisher_vd.publish(msg)
-        # self.get_logger().info('Publishing: VD infos')
+    def callback_vd(self):
+        msg = VDmeasurement()
+        msg.frame_id = self.i
+        
+        if(self.uavA.z_VD[self.i]["B detected"] is not None): # A->B
+            msg.b_detected = True
+            msg.measure_b = np.reshape(self.uavA.z_VD[self.i]["B detected"], (1,16))[0] 
+        else:
+            msg.b_detected = False
+            msg.measure_b = np.zeros((1,16),dtype=np.float64)[0]
+            
+        if(self.uavA.z_VD[self.i]["C detected"] is not None): # A->C
+            msg.c_detected = True
+            msg.measure_c = np.reshape(self.uavA.z_VD[self.i]["C detected"], (1,16))[0] 
+        else:
+            msg.c_detected = False
+            msg.measure_c = np.zeros((1,16),dtype=np.float64)[0]
+        
+        if(self.uavA.z_VD[self.i]["D detected"] is not None): # A->D
+            msg.d_detected = True
+            msg.measure_d = np.reshape(self.uavA.z_VD[self.i]["D detected"], (1,16))[0] 
+        else:
+            msg.d_detected = False
+            msg.measure_d = np.zeros((1,16),dtype=np.float64)[0]
+        
+        self.publisher_vd.publish(msg)
+        self.get_logger().info('Publishing: VD infos')
 
-    # def callback_lc(self):
-        # msg = LCmeasurement()
-        # msg.frame_id = self.i
-        # msg.measure = self.npdata
-        # self.publisher_lc.publish(msg)
-        # self.get_logger().info('Publishing: LC infos')
+    def callback_lc(self):
+        msg = LCmeasurement()
+        msg.frame_id = self.i
+        
+        #Loop closure detected for drone A Keyframe
+        if(self.uavA.z_LC[self.i]['lc detected AKF'] is not None):
+            msg.detected_akf    = True
+            msg.time_akf        = self.uavA.z_LC[self.i]['lc time AKF']
+            msg.link_akf        = self.uavA.z_LC[self.i]['lc detected AKF']
+            msg.measure_akf     = self.uavA.z_LC[self.i]['lc measure AKF']
+        else:
+            msg.detected_akf    = False
+            msg.time_akf        = 0
+            msg.link_akf        = "none"
+            msg.measure_akf     = np.zeros((1,16),dtype=np.float64)[0]
+            
+        #Loop closure detected for drone B Keyframe
+        if(self.uavA.z_LC[self.i]['lc detected BKF'] is not None):
+            msg.detected_bkf    = True
+            msg.time_bkf        = self.uavA.z_LC[self.i]['lc time BKF']
+            msg.link_bkf        = self.uavA.z_LC[self.i]['lc detected BKF']
+            msg.measure_bkf     = self.uavA.z_LC[self.i]['lc measure BKF']
+        else:
+            msg.detected_bkf    = False
+            msg.time_bkf        = 0
+            msg.link_bkf        = "none"
+            msg.measure_bkf     = np.zeros((1,16),dtype=np.float64)[0]
+            
+        #Loop closure detected for drone C Keyframe
+        if(self.uavA.z_LC[self.i]['lc detected CKF'] is not None):
+            msg.detected_ckf    = True
+            msg.time_ckf        = self.uavA.z_LC[self.i]['lc time CKF']
+            msg.link_ckf        = self.uavA.z_LC[self.i]['lc detected CKF']
+            msg.measure_ckf     = self.uavA.z_LC[self.i]['lc measure CKF']
+        else:
+            msg.detected_ckf    = False
+            msg.time_ckf        = 0
+            msg.link_ckf        = "none"
+            msg.measure_ckf     = np.zeros((1,16),dtype=np.float64)[0]
+            
+        #Loop closure detected for drone D Keyframe
+        if(self.uavA.z_LC[self.i]['lc detected DKF'] is not None):
+            msg.detected_dkf    = True
+            msg.time_dkf        = self.uavA.z_LC[self.i]['lc time DKF']
+            msg.link_dkf        = self.uavA.z_LC[self.i]['lc detected DKF']
+            msg.measure_dkf     = self.uavA.z_LC[self.i]['lc measure DKF']
+        else:
+            msg.detected_dkf    = False
+            msg.time_dkf        = 0
+            msg.link_dkf        = "none"
+            msg.measure_dkf     = np.zeros((1,16),dtype=np.float64)[0]
+        
+        self.publisher_lc.publish(msg)
+        self.get_logger().info('Publishing: LC infos')
 
 def main(args=None):
     rclpy.init(args=args)
