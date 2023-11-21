3b7d97adfa3a0f32ed7c7278fca8acb46802a75e
tschweitzer57 <thibault.schweitzer@estaca.eu>
2023-11-21 11:30:09 +0100

    debug Thread

--- swarm_backend/src/display_topics.cpp
+++ swarm_backend/src/display_topics.cpp
@@ -6,52 +6,57 @@
 
 using std::placeholders::_1;
 
-class DualSubscriber : public rclcpp::Node
+class GroundtruthSubscriber : public rclcpp::Node
 {
   public:
-    DualSubscriber()
-    : Node("dual_subscriber")
+    GroundtruthSubscriber()
+    : Node("groundtruth_subscriber")
     {
-      // Create subscriptions
-	  subscription1_ = this->create_subscription<uav_interfaces::msg::Groundtruth>(
-      "topic1", 10, std::bind(&DualSubscriber::topic_callback_gt, this, _1));
-	  
-	  subscription2_ = this->create_subscription<uav_interfaces::msg::Uwbmeasurement>(
-      "topic2", 10, std::bind(&DualSubscriber::topic_callback_uwb, this, _1));
-	  
-	  // Start threads for each callback
-	  thread1_ = std::thread([this]() { spinThread(subscription1_); });
-	  thread2_ = std::thread([this]() { spinThread(subscription2_); });
-	  
+      // Create subscription for groundtruth topic
+	  subscription_ = this->create_subscription<uav_interfaces::msg::Groundtruth>(
+      "topic1", 10, std::bind(&GroundtruthSubscriber::topic_callback_gt, this, _1));
     }
 
   private:
-    rclcpp::Subscription<uav_interfaces::msg::Groundtruth>::SharedPtr subscription1_;
-	rclcpp::Subscription<uav_interfaces::msg::Uwbmeasurement>::SharedPtr subscription2_;
-	std::thread thread1_;
-	std::thread thread2_;
+    rclcpp::Subscription<uav_interfaces::msg::Groundtruth>::SharedPtr subscription_;
 	
 	void topic_callback_gt(const uav_interfaces::msg::Groundtruth & msg) const
     {
       RCLCPP_INFO(this->get_logger(), "Frame: %d X: %f Y: %f Z:%f psi: %f", msg.frame_id, msg.x, msg.y, msg.z, msg.psi);
+    }   
+};
+
+class UwbSubscriber : public rclcpp::Node
+{
+  public:
+    UwbSubscriber()
+    : Node("uwb_subscriber")
+    {
+      // Create subscription for uwb topic
+	  subscription_ = this->create_subscription<uav_interfaces::msg::Uwbmeasurement>(
+      "topic2", 10, std::bind(&UwbSubscriber::topic_callback_uwb, this, _1));
     }
+
+  private:
+	rclcpp::Subscription<uav_interfaces::msg::Uwbmeasurement>::SharedPtr subscription_;
+	
 	void topic_callback_uwb(const uav_interfaces::msg::Uwbmeasurement & msg) const
     {
       RCLCPP_INFO(this->get_logger(), "Frame: %d B:%f C:%f D:%f", msg.frame_id, msg.dist_b, msg.dist_c, msg.dist_d);
     }
-	void spinThread(rclcpp::SubscriptionBase::SharedPtr sub) 
-	{
-	  rclcpp::spin(sub)
-	}
-    
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
+  
+  std::shared_ptr<GroundtruthSubscriber> gt_node = std::make_shared<GroundtruthSubscriber>();
+  std::shared_ptr<UwbSubscriber> uwb_node = std::make_shared<UwbSubscriber>();
+  
   rclcpp::executors::MultiThreadedExecutor executor;
-  auto node = std::make_shared<DualSubscriber>();
-  executor.add_node(node);
+  
+  executor.add_node(gt_node);
+  executor.add_node(uwb_node);
   executor.spin();
   rclcpp::shutdown();
   return 0;
